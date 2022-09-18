4bb102ee7030fee408930a97e1db7242a2b98bae
goodbyeFlash <goodbyeflash.dev@gmail.com>
2022-09-18 12:27:01 +0900

    작업 환경 구축

---
+++ src/scripts/event1.js
@@ -0,0 +1,11 @@
+// 메시지 수신 받는 eventListener 등록
+window.addEventListener('message', receiveMsgFromChild);
+
+// 자식으로부터 메시지 수신
+function receiveMsgFromChild(e) {
+  if (e.data == 6) {
+    //Todo..만점자 응모 팝업
+  } else {
+    //Todo.. 퀴즈 다시 풀어보러 가기
+  }
+}
---
+++ src/scripts/index.js
@@ -0,0 +1,1 @@
+window.location.href = 'event2.html';
