24b09489ba0b99a4fc59ee1be04e29d4f79ac8f7
yejin <kofg3137@gmail.com>
2022-09-25 01:17:23 +0900

    save

--- src/scripts/event1.js
+++ src/scripts/event1.js
@@ -1,4 +1,7 @@
 import '../asset/css/common.scss'
+import '../asset/css/popup.scss'
+
+import '../scripts/lib/jquery.min'
 
 // 메시지 수신 받는 eventListener 등록
 window.addEventListener('message', receiveMsgFromChild);
@@ -10,4 +13,25 @@ function receiveMsgFromChild(e) {
   } else {
     //Todo.. 퀴즈 다시 풀어보러 가기
   }
+
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('.popup-contents').addClass('scale');
+  }
+
+  if($('.popup').hasClass('active')){
+    $('html, body').css('overflow','hidden');
+  }else {
+    $('html, body').css('overflow','auto');
+  }
+
+  $('.popup-open').on('click', function(){
+    let targetPop = $(this).attr('data-target');
+    $('html, body').css('overflow','hidden');
+    $(targetPop).addClass('active');
+  })
+
+  $('.popup-close').on('click', function(){
+    $(this).closest('.popup').removeClass('active');
+    $('html, body').css('overflow','auto');
+  })
 }
